00b55a0eb38b2d0a3634954c4ed542913ea23c95
ajayythakur <ajay1270panwar@gmail.com>
2023-01-17 23:56:46 +0530

    React Handson 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+  state = {
+    Name: "",
+    Department: "",
+    Rating: "",
+    output: []
+  };
+
+  changeEvent = (change) => {
+    this.setState({ [change.target.name]: change.target.value })
+    // console.log(this.state);
+  };
+
+  submitHandler = (sub) => {
+    sub.preventDefault();
+    const empObj = {
+      Name: this.state.Name,
+      Department: this.state.Department,
+      Rating: this.state.Rating,
+    }
+    let arr = this.state.output;
+    arr.push(empObj);
+    this.setState({ output: arr })
+  }
+  render() {
+    return (
+      <div id='container'>
+        <h1 id="heading">
+          Employee Feedback Form
+        </h1>
+        <form>
+          <label htmlFor="Name">Name</label>
+          <input type="text" id='nameInput' name='Name' value={this.state.Name} onChange={this.changeEvent}></input>
+          <br />
+          <label htmlFor="Department">Department</label>
+          <input type="text" id='deptInput' name='Department' value={this.state.Department} onChange={this.changeEvent}></input>
+          <br />
+          <label htmlFor="Rating">Rating</label>
+          <input type="number" id='ratingInput' name='Rating' value={this.state.Rating} onChange={this.changeEvent}></input>
+          <br />
+          <button id='btn' onClick={this.submitHandler}>Submit</button>
+        </form>
+        <div id="dataShow">
+          {this.state.output.map((value, index) => {
+            return (
+              <span id="detailBox" dekey={index}>Name={value.Name} |Department={value.Department} |Rating={value.Rating} <br /></span>
+            );
+          }
+          )}
+        </div>
+      </div>
+
+    )
+  }
 }
 
-export default App;
